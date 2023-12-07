a7f7226574b07856a4d43427e4d8d249bcfa64e9
Nwokolo Matthew <markothedevmail@gmail.com>
2023-12-07 21:37:08 +0100

    added nothing to the code base just want to push

--- rel8/urls_public.py
+++ rel8/urls_public.py
@@ -20,6 +20,7 @@ from django.urls import path
 
 def trigger_error(request):
     division_by_zero = 1 / 0
+    
 urlpatterns = [
         path('sentry-debug/', trigger_error),
     path('admin/', admin.site.urls),
